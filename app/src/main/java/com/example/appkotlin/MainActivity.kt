3c5a99be41f985ca5a7cf784a077eb98a5511b51
Marcos Plata <platagonzalez2003@gmail.com>
2024-02-06 16:40:26 +0100

    Implementada la herencia de onClickListener en el MainActivity y creado el emptyActivity llamado OnBoarding. Además se ha cambiado el funcionamiento de los botones.

--- app/src/main/java/com/example/appkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.example.appkotlin
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    lateinit var tvHolaMundo: TextView
+    lateinit var rootConstrainLayout:ConstraintLayout
+    lateinit var buttonOk:Button
+    lateinit var buttonCancel:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvHolaMundo=this.findViewById(R.id.tvHolaMundo)
+        rootConstrainLayout=this.findViewById(R.id.rootConstrainLayout)
+        buttonOk=this.findViewById(R.id.buttonOk)
+        buttonCancel=this.findViewById(R.id.buttonCancel)
+
+
+        buttonOk.setOnClickListener(this)
+
+        buttonCancel.setOnClickListener(this)
+
     }
 
-    fun clickOk(v: View){
-        Log.v("MainActivity","Presioné OK")
-    }
-
-    fun clickCancel(v: View){
-        Log.v("MainActivity","Presioné Cancel")
+    override fun onClick(v: View?) {
+        if(v!!.id==R.id.buttonOk) {
+            Log.v("MainActivity","Presioné OK")
+            tvHolaMundo.text="Hasta Luego Mundo"
+            rootConstrainLayout.setBackgroundColor(Color.MAGENTA)
+        }
+        else if(v!!.id==R.id.buttonCancel){
+            Log.v("MainActivity","Presioné Cancel")
+            tvHolaMundo.text="Volvemos atrás"
+            rootConstrainLayout.setBackgroundColor(Color.YELLOW)
+        }
     }
 }
