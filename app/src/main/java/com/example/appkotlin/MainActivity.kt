91ddb2015519483b881684acaadf3cbaa843a3b5
Marcos Plata <platagonzalez2003@gmail.com>
2024-02-06 15:09:17 +0100

    Cambiado el color de fondo, añadido dos botones con funciones de onClick

--- app/src/main/java/com/example/appkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.appkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickOk(v: View){
+        Log.v("MainActivity","Presioné OK")
+    }
+
+    fun clickCancel(v: View){
+        Log.v("MainActivity","Presioné Cancel")
     }
 }
